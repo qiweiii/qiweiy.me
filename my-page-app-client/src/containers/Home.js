67d52f13da27a7b2a25616d32b7bd4218e7a45b6
qiweiii <yangqiwei97@gmail.com>
2019-05-16 00:19:38 +1000

    refactor to material style

--- my-page-app-client/src/containers/Home.js
+++ my-page-app-client/src/containers/Home.js
@@ -1,17 +1,45 @@
-import React, { Component } from "react";
-import "./Home.css";
+import React from "react";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div className="Home">
-        <div className="lander">
-          <h1>Qiwei Yang</h1>
-          <p>Penultimate year computer science student from UNSW Sydney</p>
-          <p>Looking for a software developer internship/part-time job</p>
-          <p>My email: <a href = "mailto: yangqiwei97@gmail.com">yangqiwei97@gmail.com</a></p>
-        </div>
-      </div>
-    );
+
+
+const styles = theme => ({
+  root: {
+    marginTop: theme.spacing.unit * 10,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    alignContent: 'center',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 5}px`,
+    textAlign: "center",
+    color: '#616161',
+    fontSize: 14,
+  },
+  h1: {
+    font: 'Roboto',
+    fontWeight: 300,
+    color: '#212121',
   }
+});
+
+function Home(props) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <div className={classes.root}>
+        <h1 className={classes.h1}>Qiwei Yang</h1>
+        <p>Penultimate year computer science student from UNSW Sydney</p>
+        <p>Looking for a software developer internship/part-time job</p>
+        <p>My email: <a href = "mailto: yangqiwei97@gmail.com">yangqiwei97@gmail.com</a></p>
+      </div>
+    </div>
+  );
 }
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Home);
