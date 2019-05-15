7030627f49bf9ade3a2abb87c8c9fcb9d02c9927
qiweiii <yangqiwei97@gmail.com>
2019-05-15 21:53:42 +1000

    rebuild backend and configure aws amplify

---
+++ my-page-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "qiwei-page-app-v2-dev-attachmentsbucket-wq8mmen6kpx6"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://ekrd2007ae.execute-api.ap-southeast-2.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_qbWVifwx9",
+    APP_CLIENT_ID: "4tqdidqsvsar7iocthao1960u8",
+    IDENTITY_POOL_ID: "ap-southeast-2:c3d9bdbe-ced9-44ea-b3f0-8b4515881dee"
+  }
+};
