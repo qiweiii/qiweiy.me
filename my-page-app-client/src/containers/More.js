84bc7d30bc7256ccd1edfd914e485eca17aeb05b
Qiwei Yang <yangqiwei97@gmail.com>
2019-06-08 00:35:27 +1000

    update More.js

--- my-page-app-client/src/containers/More.js
+++ my-page-app-client/src/containers/More.js
@@ -40,6 +40,7 @@ function More(props) {
       <div className={classes.root}>
         <div className={classes.text}>
           <h1 className={classes.h1}>Future</h1>
+          <p className={classes.p}>Implement algolia search.</p>
           <p className={classes.p}>Allow new users to see all blogs without logging in.</p>
           <p className={classes.p}>Improve designs.</p>
           <p className={classes.p}>Add tags to blogs, allow user to click on and see blogs under the different tags.</p>
