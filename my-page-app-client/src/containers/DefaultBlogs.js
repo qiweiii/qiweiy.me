532715d484ef3609cb4c3a45fb169ced948282a4
Qiwei Yang <yangqiwei97@gmail.com>
2019-09-02 15:42:38 +1000

    update blogs

--- my-page-app-client/src/containers/DefaultBlogs.js
+++ my-page-app-client/src/containers/DefaultBlogs.js
@@ -14,25 +14,6 @@ export const DefaultBlogs = [
   },
   {
     content:{
-      title: "How to build a VPN",
-      content: "## General steps:"
-      +"\n"+ "* Buy a VPS(Virtual Private Server) on AWS/Vultr/Azure or other platforms"
-      +"\n"+ "* Go to your VPS website, find the console of the VPS you just bought and open it"    
-      +"\n"+ "* Enter the following comments"    
-      +"\n"+ "  1. `wget ­­no­check­certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks.sh`"
-      +"\n"+ "  2. `chmod +x shadowsocks.s`"
-      +"\n"+ "  3. `./shadowsocks.sh 2>&1 | tee shadowsocks.log`"
-      +"\n"+ "* Take note of the information on the console output"
-      +"\n"+ "* Download shadowsocks here: [Shadowsocks](https://shadowsocks.org/en/download/clients.html)"
-      +"\n"+ "* Open shadowsocks and add new server with information you get in previous step",
-      author: "Qiwei",
-      image: "https://images.unsplash.com/photo-1480160734175-e2209654433c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80",
-    },
-    noteId: "how-to-build-a-vpn",
-    createdAt: Date.parse('May 15, 2019, 00:00:03'),
-  },
-  {
-    content:{
       title: "What Is My Fav Color",
       content: "`#6fcff1`"
                 + "\n" + "Google it and you will find the color"
@@ -42,21 +23,7 @@ export const DefaultBlogs = [
     },
     noteId: "fav-color",
     createdAt: Date.parse('May 15, 2019, 00:00:04'),
-  }, 
-  {
-    content:{
-      title: "Learn Serverless With AWS",
-      content: "* Serverless framework: https://serverless.com/"
-              + '\n' + "* Serverless guide on AWS: https://serverless.com/framework/docs/providers/aws/guide/"
-              + '\n' + "* Serverless tutorial: https://serverless-stack.com/#table-of-contents"
-              + '\n' + "* Serverless api example: https://github.com/serverless/examples/tree/master/aws-node-rest-api-with-dynamodb"
-              ,
-      author: "Qiwei",
-      image: "https://images.unsplash.com/photo-1536250853075-e8504ee040b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-    },
-    noteId: "cloud-computing",
-    createdAt: Date.parse('May 15, 2019, 00:00:02'),
-  }, 
+  },
   {
     content:{
       title: "How To Add Comment System To Your Website (The Simplest Way)",
@@ -113,7 +80,7 @@ export const DefaultBlogs = [
   {
     content:{
       title: "微信小程序内嵌网页",
-      content: "今天有人问我能不能把网站直接放在微信小程序里，我就想用自己的网站试试，但是发现并不可以，不过还是记录一下。首选登录微信小程序后台，页面左边选择“开发“，然后选择“开发设置” -> “业务域名” -> “开始配置” -> 添加需要访问的网站。添加的“业务域名”必须是https开头，并已经有ICP备案的。 "
+      content: "今天想要看能不能把网站直接放在微信小程序里，我就想用自己的网站试试，但是发现并不可以，不过还是记录一下。首选登录微信小程序后台，页面左边选择“开发“，然后选择“开发设置” -> “业务域名” -> “开始配置” -> 添加需要访问的网站。添加的“业务域名”必须是https开头，并已经有ICP备案的。 "
               + '\n' + "然后在小程序DevTool里添加一个page文件夹，该wxml文件里面只需要一行code："
               + '\n' + '\n' + "```" 
               + '\n' + "<web-view src=\"https://yoursite.xxx\" />" 
