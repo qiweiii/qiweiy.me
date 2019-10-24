417e6a6ff73f7d03d49fb5202985edbb56260559
Qiwei Yang <yangqiwei97@gmail.com>
2019-10-24 23:19:55 +1100

    try circleci

---
+++ my-page-app-client/bucket.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import config from "./src/config";
+
+console.log(config.s3.BUCKET);
--- my-page-app-client/src/containers/Home.js
+++ my-page-app-client/src/containers/Home.js
@@ -77,7 +77,7 @@ function Home(props) {
           <Avatar alt="Qiwei Y" src={zima} className={classes.avatar} />
           <Typography variant='h4' className={classes.name}>Qiwei Yang</Typography>
         </Grid>
-        <Typography gutterBottom variant='body1' className={classes.p}>Penultimate year Computer Science student from UNSW Sydney / Part-time Developer <br />with an interest in web technologies, cloud platforms, data management, and machine learning. </Typography>
+        <Typography gutterBottom variant='body1' className={classes.p}>Penultimate year Computer Science student from UNSW Sydney / Part-time Developer <br />with an interest in web technologies, cloud platforms and data management </Typography>
         <Typography gutterBottom variant='body1' className={classes.p}>Looking for a software engineer internship (2020 Sep - 2021 Feb)</Typography>
         <Typography gutterBottom variant='body1' className={classes.p}>My email: <a href = "mailto: yangqiwei97@gmail.com">yangqiwei97@gmail.com</a></Typography>
       </Paper>
