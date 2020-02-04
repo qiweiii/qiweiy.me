d8efa5c6ab472170b24eeeec205cd3ab22ac4d19
qiweiii <yangqiwei97@gmail.com>
2020-02-04 21:49:48 +0800

    update resume

--- my-page-app-client/src/containers/Home.js
+++ my-page-app-client/src/containers/Home.js
@@ -77,7 +77,7 @@ function Home(props) {
           <Avatar alt="Qiwei Y" src={zima} className={classes.avatar} />
           <Typography variant='h4' className={classes.name}>Qiwei Yang</Typography>
         </Grid>
-        <Typography gutterBottom variant='body1' className={classes.p}>Penultimate year Computer Science student from UNSW Sydney / Part-time Node.js Developer <br />with an interest in web technologies and cloud computing. </Typography>
+        <Typography gutterBottom variant='body1' className={classes.p}>Third year Computer Science student from UNSW Sydney / Part-time Node.js Developer <br />with an interest in web technologies and cloud computing. </Typography>
         <Typography gutterBottom variant='body1' className={classes.p}>Looking for a software engineer internship (2020 Sep - 2021 Feb)</Typography>
         <Typography gutterBottom variant='body1' className={classes.p}>My email: <a href = "mailto: yangqiwei97@gmail.com">yangqiwei97@gmail.com</a></Typography>
       </Paper>
