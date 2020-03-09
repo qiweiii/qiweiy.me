5329398c8753e03d19997ab721c828eab6af85f3
qiweiii <yangqiwei97@gmail.com>
2020-03-09 14:26:30 +0800

    change to official blue color

--- my-page-app-client/src/containers/BlogCard.js
+++ my-page-app-client/src/containers/BlogCard.js
@@ -9,7 +9,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Link as RouterLink} from 'react-router-dom'
-import zima from "../img/zima.png";
+import zima from "../img/zima.jpg";
 import blank from "../img/blank.jpg";
 
 
