13f87e940fe7dff8da9f7c9bc7528a7819f151d1
Qiwei Yang <yangqiwei97@gmail.com>
2021-04-06 15:20:37 +0800

    use redux in functional component correctly

--- my-page-app-client/src/Main.js
+++ my-page-app-client/src/Main.js
@@ -1,15 +1,22 @@
 import React, { useEffect } from 'react';
 import { getAllBlogs, saveTags } from './actions';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Routes from './route/Routes';
 
-/* load blogs asap, so user don't need to wait everytime when go to blogs */
-function Main({ getAllBlogs, allBlogsReady, allBlogs, saveTags }) {
+// load blogs asap, so user don't need to wait everytime when go to blogs
+function Main() {
+  const allBlogs = useSelector(state => state.allBlogs);
+  const allBlogsReady = useSelector(state => state.allBlogsReady);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    // console.log(childProps.isAuthenticated);
-    getAllBlogs();
-    // get all tags
+    // get all blogs
+    dispatch(getAllBlogs());
+  }, [dispatch]);
+
+  useEffect(() => {
     if (allBlogsReady) {
+      // get all tags
       let alltags = new Set();
       for (const blog of [...allBlogs]) {
         if (blog.content.tags) { // some early blogs did not have tags (value is undefined)
@@ -18,24 +25,14 @@ function Main({ getAllBlogs, allBlogsReady, allBlogs, saveTags }) {
           });
         }
       }
-      saveTags(Array.from(alltags));
+      // save all tags
+      dispatch(saveTags(Array.from(alltags)))
     }
-  // eslint-disable-next-line 
-  }, [allBlogsReady]);
+  }, [allBlogsReady, allBlogs, dispatch])
 
   return (
     <Routes />
   );
 }
 
-const mapStateToProps = state => {
-  return { 
-    allBlogs: state.allBlogs,
-    allBlogsReady: state.allBlogsReady
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  { getAllBlogs, saveTags }
-)(Main);
+export default Main;
